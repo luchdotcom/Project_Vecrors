a88c89e3b02aa2319ce039d662eed0ef4632c003
Lachezar <lachezarivanov05@gmail.com>
2020-05-28 00:17:13 +0300

    declaration new function

--- Vector.h
+++ Vector.h
@@ -21,11 +21,16 @@ public:
     double lengthVector(const Point&,const Point&);
     Vector directionVector(double ,double ,double );
     static double lengthVector(double ,double ,double ) ;
-    int sumVector(const Point&,const Point&);//???
     bool nullVector()const ;
-    bool parallelInVectors(const Vector&)const ;
+    bool parallelInVectors(const Vector&)const ; //todo
 
      bool perpendicular(const Vector&)const ;
+     Vector operator+(Vector&);//todo
+     Vector operator-(Vector&);//todo
+     double operator*(Vector&);//todo
+     Vector operator*(int);//todo
+     Vector operator^(Vector&);//todo
+     double operator()(Vector&, Vector&);//todo
 
 
 
